f1a716745831ce27edd7acb2f309e65897643303
Emilene Goulart <emilene.goulart@sap.com>
2024-08-29 21:41:36 -0300

    http method delete

---
+++ model/usuario.go
@@ -0,0 +1,11 @@
+package model
+
+type Usuario struct {
+	ID          int64
+	Nome        string
+	NomeUsuario string
+	Senha       string
+	Email       string
+	Telefone    string
+	Endereco    string
+}
